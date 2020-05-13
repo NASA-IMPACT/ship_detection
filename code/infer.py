353e7ef7ce60c51a19d3a94e9e7bdbe5c2302ab9
xhagrg <grg.iksha@hotmail.com>
2020-05-13 18:37:00 -0500

    Indent using 4 spaces.

--- code/infer.py
+++ code/infer.py
@@ -19,56 +19,54 @@ WMTS_URL = f"https://tiles1.planet.com/data/v1/PSScene3Band/{{}}/{ZOOM_LEVEL}/{{
 class Infer:
 
 
-  def __init__(self, date, weight_path=WEIGHT_FILE, credential=None):
-    self.start_date_time, self.end_date_time = self.prepare_date(date)
-    self.weight_path = weight_path
-    self.model = self.prepare_model()
-    self.credential = credential
-    self.planet_downloader = PlanetDownloader(credential)
+    def __init__(self, date, weight_path=WEIGHT_FILE, credential=None):
+        self.start_date_time, self.end_date_time = self.prepare_date(date)
+        self.weight_path = weight_path
+        self.model = self.prepare_model()
+        self.credential = credential
+        self.planet_downloader = PlanetDownloader(credential)
 
 
-  def prepare_date(self, date):
-    return [f"{date}T00:00:00Z", f"{date}T23:59:59Z"]
+    def prepare_date(self, date):
+        return [f"{date}T00:00:00Z", f"{date}T23:59:59Z"]
 
 
-  def prepare_model(self):
-    return load_from_path(self.weight_path)
+    def prepare_model(self):
+        return load_from_path(self.weight_path)
 
 
-  def infer(self):
-    for location, extent  in EXTENTS.items():
-      print('running for location:', location)
-      datasets = self.prepare_dataset(extent)
-      for dataset in datasets:
-        predictions = self.model.predict(dataset['images'])
-        rows, columns = [elem[1] - elem[0] for elem in dataset['tiles']]
-        predictions = predictions.reshape((rows, columns, IMG_SIZE, IMG_SIZE))
-        # insert here
-
-
-  def prepare_dataset(self, extent):
-    items = self.planet_downloader.search_ids(
-      extent, self.start_date_time, self.end_date_time
-    )
-    for item in items:
-      x_indices, y_indices = item['tiles']
-      print(x_indices[1] - x_indices[0])
-      print(y_indices[1] - y_indices[0])
-      for x_index in list(range(*x_indices)):
-        for y_index in list(range(*y_indices)):
-          response = requests.get(
-            WMTS_URL.format(item['id'], x_index, y_index, self.credential)
+    def infer(self):
+        for location, extent  in EXTENTS.items():
+          print('running for location:', location)
+          items = self.planet_downloader.search_ids(
+            extent, self.start_date_time, self.end_date_time
           )
-          response.raise_for_status()
-          img = np.asarray(
-            Image.open(BytesIO(response.content)).resize(
-                (IMG_SIZE, IMG_SIZE)
-              ).convert('RGB')
+          for item in items:
+            images = self.prepare_dataset(item['tiles'], item['id'])
+            predictions = self.model.predict(images)
+            columns, rows = [elem[1] - elem[0] for elem in dataset['tiles']]
+            predictions = predictions.reshape((rows, columns, IMG_SIZE, IMG_SIZE))
+            # insert here
+
+
+    def prepare_dataset(self, tile_range, tile_id):
+        x_indices, y_indices = tile_range
+        print(x_indices, y_indices)
+        images = list()
+        for x_index in list(range(*x_indices)):
+          for y_index in list(range(*y_indices)):
+            response = requests.get(
+              WMTS_URL.format(tile_id, x_index, y_index, self.credential)
             )
-          item['images'].append(img)
-      item['images'] = np.asarray(item['images'])
-    return items
+            response.raise_for_status()
+            img = np.asarray(
+              Image.open(BytesIO(response.content)).resize(
+                  (IMG_SIZE, IMG_SIZE)
+                ).convert('RGB')
+              )
+            images.append(img)
+        return np.asarray(images)
 
 
-  def prepare_geojson(self):
-    pass
+    def prepare_geojson(self):
+        pass
